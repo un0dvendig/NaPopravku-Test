982f3b41007aa63f1d8dd8fc6c34eb85c3efe743
Eugene Ilyin <evgen.ilyin@gmail.com>
2020-06-17 10:44:49 +0300

    Setup for iPhones with iOS 12.0+

---
+++ NaPopravku Test/Resources/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  NaPopravku Test
+//
+//  Created by Eugene Ilyin on 17.06.2020.
+//  Copyright © 2020 Eugene Ilyin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: - Properties
+    
+    // MARK: - UIApplicationDelegate methods
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
